322ca10538a813f8b87e65138bc4f0e0a4185d0d
b2un0 <branko.wilhelm@gmail.com>
2014-12-21 20:57:19 +0100

    enh: phpdoc usage

--- mod_world_of_logs.php
+++ mod_world_of_logs.php
@@ -5,6 +5,9 @@
  * @link       http://www.z-index.net
  * @copyright  (c) 2011 - 2015 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @var        stdClass $module
+ * @var        Joomla\Registry\Registry $params
  */
 
 defined('_JEXEC') or die;
